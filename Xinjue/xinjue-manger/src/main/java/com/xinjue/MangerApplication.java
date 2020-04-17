753207dcf06577dc36862b76f8d82fd108f5730a
tanqingfu <1902952234@qq.com>
2020-04-17 15:59:47 +0800

    springboot分布式项目框架

--- Xinjue/xinjue-manger/src/main/java/com/xinjue/MangerApplication.java
+++ Xinjue/xinjue-manger/src/main/java/com/xinjue/MangerApplication.java
@@ -1,9 +1,15 @@
 package com.xinjue;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan(basePackages="com.xinjue.dao")
+@EnableScheduling
+@ServletComponentScan //在springBoot启动时会扫描@WebServlet，并将该类实例化  用于生产验证码图片的
 public class MangerApplication {
     public static void main(String[] args)
     {
--- Xinjue/xinjue-manger/src/main/java/com/xinjue/controller/IndexController.java
+++ Xinjue/xinjue-manger/src/main/java/com/xinjue/controller/IndexController.java
@@ -1,18 +1,30 @@
 package com.xinjue.controller;
 
 
+import com.xinjue.common.BaseController;
+import com.xinjue.common.InquiryCondition;
+import com.xinjue.meta.User;
+import com.xinjue.service.userService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
-public class IndexController {
-
+public class IndexController extends BaseController {
+    @Resource
+    userService userService;
     @RequestMapping(value = "index")
     public  String index(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        InquiryCondition InquiryCondition = new InquiryCondition();
+        int total = userService.countALL(InquiryCondition);
+        setPagePrmt_line(request, InquiryCondition, total, "1", 10);
+        List<User> list = userService.selectAll(InquiryCondition);
+        request.setAttribute("list",list);
         return "index";
     }
 }
